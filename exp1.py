c8cd9db4f9472dc1a925f5e4c959dede688a5d67
rishithapandu <punnakurishitha567@gmail.com>
2026-07-24 09:35:43 +0530

    Create exp1.py

---
+++ exp1.py
@@ -0,0 +1,13 @@
+# Import required libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib
+import sklearn
+
+# Display version of each library
+
+print("NumPy Version:", np.__version__)
+print("Pandas Version:", pd.__version__)
+print("Matplotlib Version:", matplotlib.__version__)
+print("Scikit-learn Version:", sklearn.__version__)
